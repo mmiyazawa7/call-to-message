2ee7b818c0123e5a6238d1a450c4079f3e7a33e7
Masayuki MIyazawa <mmiyazawa777@hotmail.com>
2019-05-24 19:31:25 +0900

    WhatsApp OptIn link change

--- call-to-whatsapp.py
+++ call-to-whatsapp.py
@@ -210,9 +210,10 @@ def message_status():
     print ("** message_status **")
     data = request.get_json()
     pprint(data)
-    pprint(data['error']['code'])
+    # pprint(data['error']['code'])
+    
     if data['error']['code'] == 1340:     # Sent Outside Allowed Window
-        wa_optin = "https://wa.me/" + from_whatsapp + "&text=OPTIN"
+        wa_optin = "https://wa.me/" + from_whatsapp + "?text=OPTIN"
         sms_text = "WhatsAppにメッセージを送ってよろしければ、こちらのリンクをクリックしてください。 " + wa_optin
         response_SMS = client_sms.send_message({'from': 'Nexmo', 'to': session['from'], 'type':'unicode','text': sms_text},)
         print(response_SMS)
